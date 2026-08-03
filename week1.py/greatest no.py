b32ed588d87cfabd9f07ec101d0df6cb7ed9b663
V. PUTHIYA KARTHICK <dharand041@gmail.com>
2026-08-03 18:43:40 +0530

    Add greatest no

---
+++ week1.py/greatest no.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter the value:"))
+num2 = int(input("Enter the value:"))
+
+if (num1>num2):
+    print("The num1 is greater")
+
+elif (num1<num2):
+    print("The num2 is greater")
+
+else:
+    print("The both numbers are equal")
