7870ec29ece267630a0a2d3bf4aa7eab736818be
V. PUTHIYA KARTHICK <dharand041@gmail.com>
2026-08-03 18:03:47 +0530

    Add initial Python to check if a number is positive, negative, or neither

---
+++ week1.py/positive or negative.py
@@ -0,0 +1,11 @@
+num = int(input("Enter Your Number:"))
+
+if(num>0):
+    print("The Number is Positive")
+
+elif(num<0):
+    print("The Number is Negative")
+
+else:
+    print("The Number is Neither Positive nor Negative")
+    
