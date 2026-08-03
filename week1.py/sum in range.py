188069fc5a5ed97d68b18aaab75dcb069cfddd17
V. PUTHIYA KARTHICK <dharand041@gmail.com>
2026-08-03 18:37:26 +0530

    Add sum in range

---
+++ week1.py/sum in range.py
@@ -0,0 +1,11 @@
+start = int(input("Enter the value to start from:"))
+end = int(input("Enter the value to end:"))
+
+sum = 0
+
+for i in range (start,end+1):
+
+    sum += i
+
+    print(sum)
+     
